d019dda1a7bdae5ea5c54db5935ac33bd68269f3
Rui Han <ruixhan@gmail.com>
2022-05-16 17:00:09 +0800

    add routing

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -10,7 +10,7 @@ function App() {
 return (
     <Router>
     <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/Settings' element={<Settings/>} />
         <Route path='/Game' element={<Game/>} />
     </Routes>
